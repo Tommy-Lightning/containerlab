110bb6c2417ed2838afa9126970a7feb69b26825
Tommy Lightning <thomashallsmith@outlook.com>
2023-11-02 09:28:16 +0000

    updates to topology and configuration files

--- nokia_lab_build.sh
+++ nokia_lab_build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Run this file from the top level directory in the repo
-# Install Docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+# Add Docker's official GPG key
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Add the repository to Apt sources
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 # Install Containerlab
-sudo bash -c "$(cur; -sL https://get.containerlab.dev)"
+sudo bash -c "$(curl -sL https://get.containerlab.dev)"
 # Create sshubuntu docker container from file
 sudo docker build -t sshubuntu .
 # Start lab
